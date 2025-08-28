9e88b697ec09a048334ea3890997c7e064498c58
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-08-29 01:09:49 +0330

    refactor(main): update FastAPI main application

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI, status
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from app.api.v1.endpoints import router
+from app.core.exception_handlers import register_exception_handlers
+from app.infrastructure.database import initialize_database
 
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    await initialize_database()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+app.include_router(router)
+
+register_exception_handlers(app)
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def root():
