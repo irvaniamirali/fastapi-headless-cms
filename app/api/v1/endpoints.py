8335ccff753198bf1a5c840f58e4866d004f2519
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-08-29 01:09:49 +0330

    refactor(api): include routers in main endpoints

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -1,5 +1,12 @@
 from fastapi import APIRouter
 
+from app.domain.auth.api import router as auth_router
+from app.domain.user.api import router as user_router
+
 router = APIRouter(prefix="/v1")
 
 """Include to router all api rest routes with version prefix"""
+
+
+router.include_router(auth_router)
+router.include_router(user_router)
