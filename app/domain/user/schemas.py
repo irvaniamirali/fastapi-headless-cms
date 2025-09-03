afc10b18e5bbe427e0b8587d155ce697690d65c9
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-04 01:45:54 +0330

    feat(user): update user schemas - Added UserCreate and UserRead models - Improved validation

---
+++ app/domain/user/schemas.py
@@ -0,0 +1,20 @@
+from typing import TypeAlias, Annotated
+
+from pydantic import BaseModel, EmailStr, ConfigDict, Field, AfterValidator
+
+
+Email: TypeAlias = Annotated[EmailStr, AfterValidator(str.lower)]
+
+
+class UserCreate(BaseModel):
+    email: Email
+    password: str = Field(..., min_length=8)
+
+
+class UserRead(BaseModel):
+    id: int
+    email: Email
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
