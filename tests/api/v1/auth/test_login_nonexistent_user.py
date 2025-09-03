b700278ab96036b2552be65df9e3a48ab09e0395
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-04 01:54:01 +0330

    test(auth): test login with nonexistent user

---
+++ tests/api/v1/auth/test_login_nonexistent_user.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_login_nonexistent_user(http_client):
+    response = await http_client.post(
+        "/v1/auth/login",
+        data={
+            "username": "notfound@example.com",
+            "password": "AnyPassword"
+        }
+    )
+    assert response.status_code == 401
+    assert response.json()["error"]["message"] == "Invalid credentials"
