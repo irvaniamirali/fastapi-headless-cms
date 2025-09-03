17d821ba997ff26e6109bebc7ed6bab5b130105b
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-04 01:54:01 +0330

    test(user): test registration with duplicate email

---
+++ tests/api/v1/user/test_register_user_duplicate_email.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_register_user_duplicate_email(http_client):
+    # First registration
+    await http_client.post(
+        "/v1/users/register",
+        json={
+            "email": "duplicate@example.com",
+            "password": "StrongPassword123!"
+        }
+    )
+    # Duplicate registration
+    response = await http_client.post(
+        "/v1/users/register",
+        json={
+            "email": "duplicate@example.com",
+            "password": "StrongPassword123!"
+        }
+    )
+    assert response.status_code == 409
+    assert response.json()["error"]["message"] == "Email already registered."
