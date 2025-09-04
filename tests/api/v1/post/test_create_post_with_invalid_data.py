ec0fafe17c594112949df600de0bb4f7ba6b1a5b
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-04 22:01:52 +0330

    test: validate post creation fails with invalid data

---
+++ tests/api/v1/post/test_create_post_with_invalid_data.py
@@ -0,0 +1,13 @@
+import pytest
+
+from app.utils.jwt import create_access_token
+
+
+@pytest.mark.asyncio
+async def test_create_post_with_invalid_data(http_client, registered_user):
+    access_token = create_access_token(registered_user.email)
+    headers = {"Authorization": f"Bearer {access_token}"}
+    # Missing title and content
+    data = {"title": "", "content": ""}
+    response = await http_client.post("/v1/posts/", json=data, headers=headers)
+    assert response.status_code == 422
