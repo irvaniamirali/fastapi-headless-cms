c6b69c9ee7727b022abb3ab1c20e28b22363a8a7
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-09 03:18:58 +0330

    refactor(repositories): simplify repository initialization and methods

--- app/domain/user/repositories/user.py
+++ app/domain/user/repositories/user.py
@@ -3,36 +3,27 @@ from typing import Any
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.infrastructure.database.base import Base
-
 from ..models import User
 from .interface import UserRepositoryInterface
 
 
 class UserRepository(UserRepositoryInterface):
-    def __init__(self, session: AsyncSession, model: type[Base]):
+    def __init__(self, session: AsyncSession):
         self.session = session
-        self.model = model
 
-    async def insert(self, user: User) -> User:
+    async def create_user(self, *, email: str, password: str) -> User:
+        user = User(email=email, password=password)
         self.session.add(user)
         await self.session.commit()
         await self.session.refresh(user)
         return user
-
-    async def exists(self, field: str, value: Any) -> bool:
-        stmt = select(self.model).where(getattr(self.model, field) == value)
-        result = await self.session.execute(stmt)
-        return result.scalar_one_or_none() is not None
 
     async def get_by_email(self, email: str) -> User | None:
         stmt = select(User).where(User.email == email)
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def create(self, *, email: str, hashed_password: str) -> User:
-        user = User(email=email, password=hashed_password)
-        self.session.add(user)
-        await self.session.commit()
-        await self.session.refresh(user)
-        return user
+    async def exists(self, field: str, value: Any) -> bool:
+        stmt = select(User).where(getattr(User, field) == value)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none() is not None
