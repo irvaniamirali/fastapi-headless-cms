94c2daaf442bffd9b4718e6c1a6859b20ccb4cf3
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-09 03:18:58 +0330

    test(comment): update assertions for new response structures and exceptions

--- tests/api/v1/comment/test_delete_comment_not_found.py
+++ tests/api/v1/comment/test_delete_comment_not_found.py
@@ -1,12 +1,13 @@
 import pytest
 
-from app.utils.jwt import create_access_token
+from app.utils.auth.jwt import create_access_token
 
 
 @pytest.mark.asyncio
 async def test_delete_comment_not_found(http_client, registered_user):
     access_token = create_access_token(registered_user.email)
     headers = {"Authorization": f"Bearer {access_token}"}
-    response = await http_client.delete("/v1/comments/99999", headers=headers)
+    comment_id = 99999
+    response = await http_client.delete(f"/v1/comments/{comment_id}", headers=headers)
     assert response.status_code == 404
-    assert response.json()["error"]["message"] == "Comment not found"
+    assert response.json()["detail"]["message"] == f"Comment with id {comment_id} was not found."
