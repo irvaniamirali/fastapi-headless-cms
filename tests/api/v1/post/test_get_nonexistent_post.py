c8334f333bce65824b68bb4a10857d2927202027
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-09 03:18:58 +0330

    test(post): update assertions for new response structures and exceptions

--- tests/api/v1/post/test_get_nonexistent_post.py
+++ tests/api/v1/post/test_get_nonexistent_post.py
@@ -3,6 +3,7 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_get_nonexistent_post(http_client):
-    response = await http_client.get("/v1/posts/99999")
+    post_id = 99999
+    response = await http_client.get(f"/v1/posts/{post_id}")
     assert response.status_code == 404
-    assert response.json()["error"]["message"] == "Post not found"
+    assert response.json()["detail"]["message"] == f"Post with id or slug '{post_id}' not found."
