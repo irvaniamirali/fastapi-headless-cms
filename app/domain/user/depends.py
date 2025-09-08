b2701cf7d9a593e3fc161f7edfe4b3042c40f5bf
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-09 03:18:57 +0330

    refactor(user): update dependencies and exception handling

--- app/domain/user/depends.py
+++ app/domain/user/depends.py
@@ -5,11 +5,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.infrastructure.database.session import get_session
 
-from .models import User
 from .repositories import UserRepository, UserRepositoryInterface
 
 
 async def get_user_repository(
     session: Annotated[AsyncSession, Depends(get_session)],
 ) -> UserRepositoryInterface:
-    return UserRepository(session, User)
+    return UserRepository(session)
