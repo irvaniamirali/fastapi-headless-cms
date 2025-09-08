1df0084c7f23be49f089c9963b67c1fb14c5d907
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-09 03:18:58 +0330

    test(post): update assertions for new response structures and exceptions

--- tests/api/v1/post/test_delete_post_not_found.py
+++ tests/api/v1/post/test_delete_post_not_found.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.utils.jwt import create_access_token
+from app.utils.auth.jwt import create_access_token
 
 
 @pytest.mark.asyncio
@@ -8,7 +8,8 @@ async def test_delete_post_not_found(http_client, registered_user):
     access_token = create_access_token(registered_user.email)
     headers = {"Authorization": f"Bearer {access_token}"}
 
-    response = await http_client.delete("/v1/posts/99999", headers=headers)
+    post_id = 99999
+    response = await http_client.delete(f"/v1/posts/{post_id}", headers=headers)
 
     assert response.status_code == 404
-    assert response.json()["error"]["message"] == "Post not found"
+    assert response.json()["detail"]["message"] == f"Post with id {post_id} not found."
