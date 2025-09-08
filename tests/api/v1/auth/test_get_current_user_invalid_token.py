f889f78534deb126e29185a9b209091fb3a42710
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-09 03:18:58 +0330

    test(auth): update assertions for new response structures and exceptions

--- tests/api/v1/auth/test_get_current_user_invalid_token.py
+++ tests/api/v1/auth/test_get_current_user_invalid_token.py
@@ -7,4 +7,4 @@ async def test_get_current_user_invalid_token(http_client):
         "/v1/auth/me", headers={"Authorization": "Bearer invalidtoken"}
     )
     assert response.status_code == 401
-    assert response.json()["detail"] == "Could not validate authentication credentials"
+    assert response.json()["detail"]["message"] == "Could not validate authentication credentials"
