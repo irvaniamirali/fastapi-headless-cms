8f5e0a973cce759076beb256d74bc2162e54ad78
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-09 03:18:58 +0330

    test(post): update assertions for new response structures and exceptions

--- tests/api/v1/post/test_get_post_by_id_success.py
+++ tests/api/v1/post/test_get_post_by_id_success.py
@@ -6,6 +6,6 @@ async def test_get_post_by_id_success(http_client, create_post_fixture):
     post = await create_post_fixture(title="Test Post By ID")
     response = await http_client.get(f"/v1/posts/{post.id}")
     assert response.status_code == 200
-    post_data = response.json()
+    post_data = response.json()["data"]
     assert post_data["title"] == "Test Post By ID"
     assert post_data["id"] == post.id
