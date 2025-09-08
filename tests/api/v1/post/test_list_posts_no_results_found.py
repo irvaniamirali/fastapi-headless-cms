8ddc01ebcf4b77e5fc9564170562f208c3681fcb
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-09-09 03:18:58 +0330

    test(post): update assertions for new response structures and exceptions

--- tests/api/v1/post/test_list_posts_no_results_found.py
+++ tests/api/v1/post/test_list_posts_no_results_found.py
@@ -5,6 +5,6 @@ import pytest
 async def test_list_posts_no_results_found(http_client):
     response = await http_client.get("/v1/posts/?search=nonexistent_search")
     assert response.status_code == 200
-    data = response.json()
+    data = response.json()["data"]
     assert data["total"] == 0
     assert len(data["items"]) == 0
